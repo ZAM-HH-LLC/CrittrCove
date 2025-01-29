02807c5a1e4a3f12057d411b6f641f88d48a76ce
aiintegrations <zam.hh.llc@gmail.com>
2025-01-28 19:16:56 -0700

    Adding some testing. to test the two i have right now run "python manage.py test tests.pro_tests.GET_Requests.get_pro_dashboard_test tests.pro_tests.GET_Requests.get_bookings_test -v 2". Added a bunch of other test directories and files. the test_runner.py is what is supposed to format the stuff in test_configs.py to do the api call and get the response, and it then outputs to the test_output file in the tests directory. test_output still has some formatting issues, it isn't showing the individual tests being run and/or passed/failed

--- backend/zenexotics_backend/clients/models.py
+++ backend/zenexotics_backend/clients/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.conf import settings
 
+def default_list():
+    return []
+
+def default_dict():
+    return {}
+
 class Client(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
@@ -9,12 +15,12 @@ class Client(models.Model):
     )
     about_me = models.TextField(blank=True)
     emergency_contact = models.JSONField(
-        default=dict,
+        default=default_dict,
         blank=True,
         help_text='List of emergency contacts'
     )
     authorized_household_members = models.JSONField(
-        default=list,
+        default=default_list,
         blank=True,
         help_text='List of people authorized to interact with the pet'
     )
--- backend/zenexotics_backend/manage.py
+++ backend/zenexotics_backend/manage.py
@@ -6,7 +6,12 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zenexotics_backend.settings")
+    # Set the default Django settings module
+    if 'test' in sys.argv:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zenexotics_backend.test_settings')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zenexotics_backend.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
